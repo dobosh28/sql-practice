504f388104dea68192f53328f8fa93cb97b91af9
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-07-25 13:45:18 -0400

    solve alien_cast

--- lib/06_joins_warmup.rb
+++ lib/06_joins_warmup.rb
@@ -134,5 +134,20 @@ end
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
+    SELECT  
+      name
+    FROM
+      actors
+    JOIN
+      castings ON actors.id = castings.actor_id
+    WHERE
+      castings.movie_id = (
+        SELECT  
+          id
+        FROM
+          movies
+        WHERE
+          title = ('Alien')
+      )
   SQL
 end
