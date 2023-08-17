42c223336942817a593b2585d5eba5594cfdc8c8
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-08-17 18:38:37 -0400

    solve teachers_and_mobiles

--- lib/08_using_null.rb
+++ lib/08_using_null.rb
@@ -59,6 +59,10 @@ def teachers_and_mobiles
   # 444 2266' if no number is given. Show teacher name and mobile
   # #number or '07986 444 2266'
   execute(<<-SQL)
+    SELECT 
+      name, COALESCE(mobile, '07986 444 2266')
+    FROM
+      teachers
   SQL
 end
 
