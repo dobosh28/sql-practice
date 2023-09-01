5e602c66f58643b924f618d8151bcfccbc0010e5
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-08-31 22:41:34 -0400

    solve start_at_craiglockhart

--- lib/09_self_joins.rb
+++ lib/09_self_joins.rb
@@ -198,6 +198,18 @@ def start_at_craiglockhart
   # by taking one bus, including 'Craiglockhart' itself. Include the stop name,
   # as well as the company and bus no. of the relevant service.
   execute(<<-SQL)
+    SELECT
+      DISTINCT stopb.name, b.company, b.num
+    FROM
+      routes a
+    JOIN
+      routes b ON (a.company = b.company AND a.num = b.num)
+    JOIN
+      stops stopa ON (a.stop_id = stopa.id)
+    JOIN
+      stops stopb ON (b.stop_id = stopb.id)
+    WHERE
+      stopa.name = 'Craiglockhart'
   SQL
 end
 
